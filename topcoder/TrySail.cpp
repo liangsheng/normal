09b6dd3374cdc57cd138e9f938db6fb83990095a
liangsheng <904491908@qq.com>
2016-07-10 08:32:01 +0800

    2016_07_10

---
+++ topcoder/TrySail.cpp
@@ -0,0 +1,107 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "TrySail.cpp"
+#include <cstdlib>
+#include <cctype>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <fstream>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <list>
+#include <stdexcept>
+#include <functional>
+#include <utility>
+#include <ctime>
+#define eps 1e-8
+#define zero(x) (((x)>0?(x):-(x))<eps)
+
+#define pause cout << " press ansy key to continue...",  cin >> chh
+#define file_r(x) freopen(x,  "r",  stdin)
+#define file_w(x) freopen(x,  "w",  stdout)
+#define lowbit(x) ((x) & (-x))
+#define repit(i, c) for (__typeof__((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define repe(i, u) for (int i = head[u]; i != -1; i = nxt[i])
+#define repd(i, n) for (int i = (n - 1); i >= 0; i--)
+#define FOR(i, n, m) for (int i = (n); i <= (m); i++)
+#define FORD(i, n, m) for (int i = (n); i >= (m); i--)
+#define pb push_back
+#define X first
+#define Y second
+#define ins insert
+#define rb rbegin
+#define be begin
+#define er erase
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define SZ(c) (c).size()
+#define ALL(c) (c).begin(), (c).end()
+#define sqr(r) ((r) * (r))
+#define dis(x1, y1, x2, y2) (((x1) - (x2)) * ((x1) - (x2)) + ((y1) - (y2)) * ((y1) - (y2)))
+#define FASTIO ios::sync_with_stdio(false);cin.tie(0)
+
+#define sc(x) cout << #x" = " << x << endl, system("pause")
+#define sc2(x, y) cout << #x" = " << x << " " << #y" = " << y << endl, system("pause")
+#define sc3(x, y, z) cout << #x" = " << x << " " << #y" = " << y << " " << #z" = " << z << endl, system("pause")
+#define sc4(x, y, z, w) cout << #x" = " << x << " " << #y" = " << y << " " << #z" = " << z << " " << #w" = " << w << endl, system("pause")
+
+#define in(n) scanf("%d", &n)
+#define in2(n, m) scanf("%d %d", &n, &m)
+#define in3(x, y, z) scanf("%d %d %d", &x, &y, &z)
+
+using namespace std;
+int chh;
+
+typedef vector<int> vi;
+typedef set<int> si;
+typedef map<int, int> mii;
+typedef pair<int, int> pii;
+typedef pair<int, pii> pi3;
+typedef vector< pair<int, int> > vpii;
+typedef long long LL;
+
+const int N = 55;
+
+class TrySail {
+    public:
+    int get(vector <int> a) {
+
+    }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {2,3,3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 8; verify_case(0, Arg1, get(Arg0)); }
+	void test_case_1() { int Arr0[] = {7,3,5,2}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 17; verify_case(1, Arg1, get(Arg0)); }
+	void test_case_2() { int Arr0[] = {13,13,13,13,13,13,13,13}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 26; verify_case(2, Arg1, get(Arg0)); }
+	void test_case_3() { int Arr0[] = {114,51,4,191,9,81,0,89,3}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 470; verify_case(3, Arg1, get(Arg0)); }
+	void test_case_4() { int Arr0[] = {108,66,45,82,163,30,83,244,200,216,241,249,89,128,36,28,250,190,70,95,117,196,19,160,255,129,10,109,189,24,22,25,134,144,110,15,235,205,186,103,116,191,119,183,45,217,156,44,97,197}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 567; verify_case(4, Arg1, get(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE
+int main() {
+        TrySail ___test;
+        ___test.run_test(-1);
+        return 0;
+}
+// END CUT HERE
